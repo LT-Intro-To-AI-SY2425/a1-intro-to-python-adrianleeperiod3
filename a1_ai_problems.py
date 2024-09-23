90c78b5aa71486139cb69802340b59142e306f8a
Adrian Lee <ajlee4@cps.edu>
2024-09-23 09:53:22 -0500

    i am trying to push this now because it wont sync

--- a1_ai_problems.py
+++ a1_ai_problems.py
@@ -1,5 +1,6 @@
+import math
 
-def fizbuzz(input_num):
+"""def fizbuzz(input_num):
     if(input_num%3==0):
         if(input_num%5==0):
             return 'FizzBuzz'
@@ -14,5 +15,58 @@ assert fizbuzz(3) == "Fizz", "fizzbuzz 3 test"
 assert fizbuzz(4) == 4, "fizzbuzz 4 test"
 assert fizbuzz(5) == "Buzz", "fizzbuzz 5 test"
 assert fizbuzz(6) == "Fizz", "fizzbuzz 6 test"
-assert fizbuzz(15) == "FizzBuzz", "fizzbuzz 15 test"
+assert fizbuzz(15) == "FizzBuzz", "fizzbuzz 15 test" """
 
+"""3. Simple Calculator
+Build a simple calculator that can perform basic arithmetic operations: addition, subtraction, multiplication, and division. The program should ask the user for two numbers and the operation they want to perform."""
+
+# num1 = input("Enter an initial number: ")
+# num2 = input("Enter an another number: ")
+# operation = input("Enter an operation (+ - / %): ")
+
+# def calc(num1, num2, operation):
+#     if(operation == "+"):
+#         return num1 + num2
+#     elif(operation == "-"):
+#         return num1 - num2
+#     elif(operation == "/"):
+#         return num1 / num2
+#     elif(operation == "*"):
+#         return num1 * num2
+#     elif(operation == "%"):
+#         return num1 % num2
+#     else:
+#         return "Operation is not recognized"
+
+# assert calc(1,1,"+") == 2, "addition"
+# assert calc(3,1,"-") == 2, "subtraction"
+# assert calc(8,7,"*") == 56, "multiplication"
+# assert calc(21,7,"/") == 3, "division"
+# assert calc(22,7,"%") == 1, "modulus"
+# print("Tests Passed")
+
+# def sinApprox(argument):
+#     sum = 0
+#     for i in range (100):
+#         numerator = pow(-1,i)*pow(argument,((2*i)+1))
+#         denominator = math.factorial((2*i)+1)
+#         sum += numerator / denominator 
+#     print(sum)
+#     return sum
+
+# assert sinApprox(0) == 0, "Zero Case"
+# assert sinApprox(1.047198) == 0.5, "60 Degree Case"
+# assert sinApprox() == 0, "Zero Case"
+# print("Tests passed")
+
+def countA(str):
+    numberOfA = 0
+    for el in str:
+        if (el == "A" or el == "a"):
+            numberOfA += 1
+    return numberOfA
+
+assert countA("abacus") == 2, "ABACUS failed"
+assert countA("aaaoooAAAOOO") == 6, "oaOA failed"
+assert countA("this has no A's") == 2, "ironic sentence failed"
+print("All Tests Passed")
--- a1_writeup.md
+++ a1_writeup.md
@@ -14,10 +14,12 @@ Mr. Berg will look at all your files to determine what you have completed.  If t
 
 1. What is your initial thoughts about the python programming language?
 
-
+I find the python programming language to be quicker and more intuitive than I'd consider java to be. However since I learned java first, some aspects of python feel incorret or confusing. 
 
 2. Describe some similarities and differences that you found between python and other programming languages you have used in the past?
 
-
+Python feels more direct as to what I am trying to instruct the program to do, as Java typically requires more tedious instruction in order to complete simple tasks. Additionally, Java needs specific definitions in data types for each variable, while Python's declaration is interpretive. 
 
 3. What are some things that you learned through this assignment?
+
+Through this assignment, I learned how to translate familar aspects I learned on Java into Python code for problem solving. I also learned that Python has built-in functions that execute tasks that would otherwise take multiple steps in Java coding. Once I become more knowledgable and comfortable with Python, coding with it will likely be faster. 
